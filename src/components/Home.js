e146ecb43032022c95acf5823bb258a0e83dc737
Tanner Shilson <tannershilson@Tanners-iMac.local>
2021-05-21 11:02:54 -0700

    added local storeage and react router

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,35 +1,64 @@
 import React, { Component } from 'react'
 import Skin from './Skin';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
 
 export default class Home extends Component {
     constructor(){
         super()
         this.state={
             allSkins: {},
+            skinsloaded: false,
             showGodSkins: false,
-            godName: ""
+            godName: "",
+            timeNow:  Date.now(),
+            timeInMonth:  Date.now() + 2629800000,
+            timeInTwoWeeks:  Date.now() + 1209600033
         }
     }
 
     componentDidMount() {
+        const updateTime = localStorage.getItem('updateTime');
+        if(this.state.timeNow >  updateTime){
+          
         fetch("https://cms.smitegame.com/wp-json/smite-api/get-posts/1?tag=update-notes&per_page=10000")
           .then(res => res.json())
           .then(
             (res) => {
               this.setState({
-                isLoaded: true,
                 allUpdates: res
               });
+              localStorage.setItem('allUpdates', JSON.stringify(this.state.allUpdates));
+              localStorage.setItem('updateTime', this.state.timeInTwoWeeks);
               this.sendSlug()
             },
-           
             (error) => {
               this.setState({
-                isLoaded: true,
                 error
               });
             }
           )
+        } else{
+     
+            const allUpdates = JSON.parse(localStorage.getItem('allUpdates'));
+            this.setState({
+                allUpdates: allUpdates
+            })
+            if(this.state.allUpdates){
+                setTimeout(this.sendSlug(), 1000)
+            }else{
+                setTimeout(this.sendSlug, 4000)
+            }
+            
+        }
+
+        const godTime = localStorage.getItem('godTime');
+        if(this.state.timeNow >  godTime){
+       
         fetch("https://cms.smitegame.com/wp-json/smite-api/all-gods/1?")
             .then(res => res.json())
             .then(
@@ -37,14 +66,27 @@ export default class Home extends Component {
                   this.setState({
                     gods: res
                   });
-                  
+                  localStorage.setItem('gods', JSON.stringify(this.state.gods));
+                  localStorage.setItem('godTime', this.state.timeInMonth);
                 }
+                
             )
+        }else{
+           
+            const gods = JSON.parse(localStorage.getItem('gods'));
+            this.setState({
+                gods: gods
+            })
+        }
       }
       
     sendSlug = () =>{
+        
+        if(this.state.allUpdates ){
+        const skinTime = localStorage.getItem('skinTime');
+        if(this.state.timeNow >  skinTime){
+           
         for(let x = 0; x<this.state.allUpdates.length; x++){
-            let stateTitle = `${this.state.allUpdates[x].title}${this.state.allUpdates[x].id}`
         if(this.state.allUpdates[x].real_categories.includes('PC')){
             if(this.state.allUpdates[x].id === 15808){ continue }else{
         fetch(`https://cms.smitegame.com/wp-json/smite-api/get-post/1?slug=${this.state.allUpdates[x].slug}`)
@@ -111,17 +153,12 @@ export default class Home extends Component {
                 temparray[4] = onlyURL;
 
                 
-                
-                
                 let tempState = `${x}${i}`
                 this.setState({
                     allSkins: {...this.state.allSkins, [tempState]: temparray}
                   });
+                localStorage.setItem('allSkins', JSON.stringify({...this.state.allSkins, [tempState]: temparray}));
                 }
-            this.setState({
-                [stateTitle]: {...res, new: splitt, filteredArr: filtered}
-            })
-            
             },
            
             (error) => {
@@ -130,7 +167,27 @@ export default class Home extends Component {
           )
         }
     }
+    if(x === this.state.allUpdates.length-1){
+     
+        //localStorage.setItem('allSkins', JSON.stringify(this.state.allSkins));
+        localStorage.setItem('skinTime', this.state.timeInTwoWeeks);
+        setTimeout(this.setState({skinsloaded: true}), 3000)
+        
     }
+    }
+}else{
+  
+    const allSkins = JSON.parse(localStorage.getItem('allSkins'));
+    this.setState({
+        allSkins: allSkins,
+        allUpdatesLoaded: true,
+        skinsloaded: true
+    })
+    
+}
+}else{
+    setTimeout(this.sendSlug(), 10000)
+}
     }
 
     setGodName = (name) =>{
@@ -148,29 +205,39 @@ export default class Home extends Component {
     }
     render() {
         return (
+            <Router>
+            <Switch>
+                
             <div>
+            
                 {this.state.showGodSkins ?(
+                <Route path="/skins">
                     <div>
                     <Skin allSkins={this.state.allSkins} godName={this.state.godName} goBack={this.goBack}/>
                     </div>
-                ):(this.state.gods &&<div >
+                </Route>):( this.state.gods && this.state.skinsloaded?(<Route path="/"> <div >
+            
+                    <div>
                     <h1 className="header">Smite Card Art Wallpapers</h1>
                     <div className="home">
-                    {this.state.gods.map((god, i) =>(<div onClick={()=> this.setGodName(god.name)} className="single-god" key={god.id} id={god.name.replace(/\s/g, '')} >
+                    {this.state.gods.map((god, i) =>(<Link to="skins" key={god.id}><div onClick={()=> this.setGodName(god.name)} className="single-god"  id={god.name.replace(/\s/g, '')} >
                             <div className='home-godcardart'>
                             <img src={god.card.replace(/[']/g, '')} alt={god.name}/>
                             </div>
                             <div className='god-name'>
                             <p onClick={()=> this.setGodName(god.name)}>{god.name}</p>
                             </div>
-                   </div> ))}
+                   </div></Link> ))}
                     </div>
-                    </div>)}
+                    </div>
+                    </div></Route>): <h1 className="loading">Loading...</h1>)}
                     <footer className="footer">
                     <p>Smite Wallpapers By Tanner Shilson.</p>
                     <p>Data &amp; Art provided by Hi-Rez Studios. © Hi-Rez Studios, Inc. All rights reserved.</p>
                     </footer>
             </div>
+            </Switch>
+            </Router>
         )
     }
 }
--- src/components/Skin.js
+++ src/components/Skin.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react'
+import {
+    BrowserRouter as Router,
+ Link
+  } from "react-router-dom";
 
 export default class Skin extends Component {
     constructor(){
@@ -7,6 +11,12 @@ export default class Skin extends Component {
         
         }
     }
+
+    componentDidUpdate(){    
+        window.onpopstate = e => {
+            this.props.goBack()
+        }
+      }
 
     componentDidMount(){
         let filteredData = []
@@ -23,14 +33,16 @@ export default class Skin extends Component {
 
     render() {
         return (
+            <Router>
             <div>
                 <div className="skins-header ">
-                <p onClick={this.props.goBack}>Go Back</p>
+                    <Link to="/">
+                <h1 className="go-back" onClick={this.props.goBack}>Go Back</h1></Link>
                 <h1 className="header">{this.props.godName}</h1>
                 </div>
                 {this.state.filteredData && <div className="skins">
                 {this.state.filteredData.map((god, i) =>(
-                        <div key={god[0]} className='single-skin'>
+                        <div key={god[0]+i} className='single-skin'>
                             <div className='single-skin'>
                         <img src={god[4]} width="300" alt={god[0]}/>
                         <div className="skin-name">
@@ -41,12 +53,13 @@ export default class Skin extends Component {
                         <a href={god[1]} rel="noreferrer" target="_blank" >1920x1080</a>
                         <a href={god[2]} rel="noreferrer" target="_blank">2056x1446</a>
                         <a href={god[3]} rel="noreferrer" target="_blank">3840x2160</a>
-                        <a href={god[4]} rel="noreferrer" target="_blank">1080x1920</a>
+                        <a href={god[4]} rel="noreferrer" target="_blank">Mobile</a>
                         </div>
                         </div>
               
                 ))}</div>}
             </div>
+            </Router>
         )
     }
 }
